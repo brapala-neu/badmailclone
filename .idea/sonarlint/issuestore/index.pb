4472e435d39527cba8ab455ecab4f6a86096a1b7
bryan.rapala <bjrapala@gmail.com>
2019-01-25 15:20:49 -0500

    once more

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,5 @@ O
 src/main/java/Acct/Message.java,8\c\8c294c24f6057294d7102db11d279c945676a5a4
 Q
 !src/main/java/Acct/MailClone.java,3\0\30625b7dbd96081e078045a45c7f2e5acf09bf4c
+Q
+!src/main/java/Acct/MyServlet.java,d\b\db3c3f37c3a965012402d8a5e8203d6e5a512401
--- src/main/java/Acct/MyServlet.java
+++ src/main/java/Acct/MyServlet.java
@@ -1,4 +1,52 @@
 package main.java.Acct;
 
-public class MyServlet {
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.rmi.ServerException;
+import java.util.concurrent.Executor;
+
+public class MyServlet extends HttpServer {
+    public void bind(InetSocketAddress inetSocketAddress, int i) throws IOException {
+
+    }
+
+    public void start() {
+        
+    }
+
+    public void setExecutor(Executor executor) {
+
+    }
+
+    public Executor getExecutor() {
+        return null;
+    }
+
+    public void stop(int i) {
+
+    }
+
+    public HttpContext createContext(String s, HttpHandler httpHandler) {
+        return null;
+    }
+
+    public HttpContext createContext(String s) {
+        return null;
+    }
+
+    public void removeContext(String s) throws IllegalArgumentException {
+
+    }
+
+    public void removeContext(HttpContext httpContext) {
+
+    }
+
+    public InetSocketAddress getAddress() {
+        return null;
+    }
 }
